ef5b87ae335975c0df1ebe4c59e0911084e0eb40
segem001 <kipdom001@gmail.com>
2022-02-28 23:02:51 +0300

    add changes views.py file

--- app/views.py
+++ app/views.py
@@ -16,3 +16,8 @@ def search_results(request):
             searched_articles = Image.search_image(search_term)
             message = f"{search_term}"
 
+            return render(request, 'app/search.html',{"message":message, "articles":searched_articles})
+
+        else:
+            message= "You havent searched for any term"
+            return render(request,'app/search.html',{"message":message})
