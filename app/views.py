e99db9b363667bbcb77dedf14fdf51f828fa1b5b
segem001 <kipdom001@gmail.com>
2022-02-28 23:01:56 +0300

    modify views.py file

--- app/views.py
+++ app/views.py
@@ -4,3 +4,15 @@ from .models import Image,Location,Category
 
 
 
+def index(request):
+    all_images=Image.objects.all()
+    location=Location.objects.all()
+    context={"all_images":all_images,"locations":location}
+    return render(request,'app/index.html',context)
+
+def search_results(request):
+        if 'query'in request.GET and request.GET["query"]:
+            search_term = request.GET.get("query")
+            searched_articles = Image.search_image(search_term)
+            message = f"{search_term}"
+
