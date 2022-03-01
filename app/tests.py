9d7d9ff221d2a8e43691f5ede7ed506e43c45803
segem001 <kipdom001@gmail.com>
2022-03-01 12:58:06 +0300

    define test update method

--- app/tests.py
+++ app/tests.py
@@ -5,3 +5,31 @@ from django.test import TestCase
 from .models import Image, Location, Category
 
 
+# Create your tests here.
+class LocationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        """Creating a new location and saving it"""
+        self.place = Location(location_name='Nairobi')
+        self.place.save_location()
+
+
+    def test_instance(self):
+        """Testing instance"""
+        self.assertTrue(isinstance(self.place, Location))
+
+
+    def test_save_method(self):
+        """Testing Save Method"""
+        self.place.save_location()
+        places = Location.objects.all()
+        self.assertTrue(len(places) > 0)
+
+
+    def test_update_method(self):
+        """Testing Update Method"""
+        self.place.update_location(name="Nyeri")
+        places = Location.objects.all()
+        self.assertTrue(len(places) > 0)
+
+
